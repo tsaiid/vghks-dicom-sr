2a979182dcda41d0305b43230830f1739e2d2b42
I-Ta Tsai <ittsai@gmail.com>
2014-06-16 21:56:43 +0800

    add a temp tool to convert dcm to yaml file for debug.

---
+++ app/tool-to_yaml.rb
@@ -0,0 +1,18 @@
+require 'dicom'
+include DICOM
+require_relative 'dicom-sr-constrants.rb'
+require 'yaml'
+
+if ARGV.first.nil?
+  p "need path."
+  exit
+end
+
+path = ARGV.first
+
+# convert to yaml
+dcm = DObject.read(path)
+fname = dcm.value(AN) + ".yaml"
+File.open(fname, "w") do |f|
+  f.write dcm.to_yaml
+end
