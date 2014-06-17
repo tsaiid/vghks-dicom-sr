6d16596196012af520148e39dab7af456c545d5b
I-Ta Tsai@MBP <ittsai@gmail.com>
2014-06-17 15:36:01 +0800

    fix wrong site string. hack for ruby-unit strange behavior.

--- app/dicom-sr-standardize.rb
+++ app/dicom-sr-standardize.rb
@@ -1,35 +1,41 @@
 require 'ruby-units'
 
 def standardize_site(site)
-  return "kidney" if ["Kidney", "Renal L"].include? site
+  return "kidney" if ["Kidney", "Renal L", "Lt Renal L", "Rt Renal L"].include? site
   return "spleen" if ["Spleen", "Splenic L"].include? site
+  return "CBD" if ["CBD", "Bileduct"].include? site
   site
 end
 
 def standardize_side(side)
   return "left" if ["Left", "Lt"].include? side
-  return "left" if ["Right", "Rt"].include? side
+  return "right" if ["Right", "Rt"].include? side
   side
 end
 
 def standardize_value(site, value, unit)
   preferred_unit_by_site = {
-    "kidney" => "centimeters",
-    "spleen" => "centimeters",
-    "CBD" => "millimeters"
+    "kidney" => "cm",
+    "spleen" => "cm",
+    "CBD" => "mm"
   }
 
-  Unit(value + unit).convert_to(preferred_unit_by_site[site]).round(1).to_s
+  # strange hack, ruby-units conversion sometimes results in rational scalar?!
+  # follow this issue: https://github.com/olbrich/ruby-units/issues/102
+  converted_value = Unit(value + unit).convert_to(preferred_unit_by_site[site]).round(1)
+  converted_value.scalar.to_f.to_s + " " + preferred_unit_by_site[site]
 end
 
 def standardize_result(results)
   standardized_result = []
   results.each do |r|
     std_site = standardize_site(r[:site])
+    #std_site = r[:site]
     standardized_result << {
       site: std_site,
       side: standardize_side(r[:side]),
       value: standardize_value(std_site, r[:value], r[:unit])
+      #value: r[:value]+r[:unit]
     }
   end
   return standardized_result.empty? ? nil : standardized_result
