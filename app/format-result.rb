b7cd615bbf026afe239f14104c06769181fb94f4
I-Ta Tsai@MBP <ittsai@gmail.com>
2014-06-18 14:57:28 +0800

    change route, without /json will return plain text. It currently supports upper and lower abdomen formatting.

---
+++ app/format-result.rb
@@ -0,0 +1,50 @@
+require 'dicom'
+include DICOM
+require_relative 'dicom-sr-constrants.rb'
+
+def format_result(dcm, parsed_hash)
+  if dcm
+    study = dcm.value(SD)
+    case study
+    when "Sono, Upper abdomen", "Sono, Lower abdomen"
+      return format_upper_lower_abdomen(parsed_hash)
+    end
+  end
+  parsed_hash.to_s
+end
+
+def format_upper_lower_abdomen(result_hash)
+  str = ""
+
+  cbd = result_hash['CBD']
+  kidney = result_hash['kidney']
+  spleen = result_hash['spleen']
+  prostate = result_hash['Prostate Vol']
+
+  if kidney
+    if (kidney['right'] && kidney['left'])
+      str += "The right kidney is about #{kidney['right']} and the left about #{kidney['left']} in longitudinal length.\n"
+    elsif kidney['right']
+      str += "The right kidney is about #{kidney['right']} in longitudinal length.\n"
+    else
+      str += "The left kidney is about #{kidney['left']} in longitudinal length.\n"
+    end
+  end
+
+  if spleen
+    str += "The spleen is about #{spleen} in longitudinal length.\n"
+  end
+
+  if cbd
+    str += "The diameter of CBD is about #{cbd}.\n"
+  end
+
+  if prostate
+    p_l = result_hash['Prostate L']
+    p_h = result_hash['Prostate H']
+    p_w = result_hash['Prostate W']
+    str += "The size of prostate is about #{p_l} x #{p_h} x #{p_w}; the volume is about #{prostate}.\n"
+  end
+
+  str
+end
--- sr-app.rb
+++ sr-app.rb
@@ -6,6 +6,7 @@ require 'yaml'
 require 'json'
 require_relative 'app/parse-dcm.rb'
 require_relative 'app/dicom-sr-get-dcm-by-acc.rb'
+require_relative 'app/format-result.rb'
 
 class DicomSR < Sinatra::Base
 
@@ -29,12 +30,31 @@ class DicomSR < Sinatra::Base
     # parse dcm
     dcm_parser_status, result = parse_dcm(dcm)
 
+    # format result
+    result_text = format_result(dcm, result)
+
+    # merge status
+    #status = dcm_parser_status unless dcm_parser_status.nil?
+
+    content_type :text
+    result_text
+  end
+
+  get '/:acc_no/json' do
+    # "Hello #{params[:name]}!"
+    acc_no = params[:acc_no]
+
+    # Check if SR exists by AccNo
+    status, dcm = get_sr_dcm_by_acc_no(acc_no)
+
+    # parse dcm
+    dcm_parser_status, result = parse_dcm(dcm)
+
     # merge status
     status = dcm_parser_status unless dcm_parser_status.nil?
 
     content_type :json
     { status: status, result: result }.to_json
   end
-
 
 end
