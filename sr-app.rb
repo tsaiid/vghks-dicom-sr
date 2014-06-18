2191ddbb5dc62add89046b9daac80b537314b520
I-Ta Tsai@MBP <ittsai@gmail.com>
2014-06-18 13:12:24 +0800

    forgot to require vascular parser. the result from vascular parser is already standardized.

--- sr-app.rb
+++ sr-app.rb
@@ -11,6 +11,8 @@ require_relative 'app/dicom-sr-philips.rb'
 require_relative 'app/dicom-sr-ge.rb'
 require_relative 'app/dicom-sr-ge-r3.1.2.rb'
 require_relative 'app/dicom-sr-standardize.rb'
+require_relative 'app/dicom-sr-ge-vascular.rb'
+require_relative 'app/dicom-sr-ph-vascular.rb'
 require_relative 'app/dicom-sr-get-dcm-by-acc.rb'
 
 class DicomSR < Sinatra::Base
@@ -35,20 +37,42 @@ class DicomSR < Sinatra::Base
     # parse dcm
     if dcm
       manufacturer = dcm[Ma].value
+      study = dcm[SD].value
 
-      case manufacturer
-      when "Philips Medical Systems"
-        result = pms_get_all_measurements(dcm)
-      when "GE Medical Systems"
-        result = gms_get_all_measurements(dcm[CS])
-      when "GE Healthcare"
-        result = gh_get_all_measurements(dcm[CS])
-      else
-        status = { error: "1", message: "#{manufacturer} is not supported yet." }
+      case study
+      when 'Sono & Doppler, Carotid art.-PCU' # vascular
+        case manufacturer
+        when "Philips Medical Systems"
+          result = phv_get_all_measurements(dcm[CS])
+        else
+          result = gev_get_all_measurements(dcm[CS])
+        end
+      else  # others
+        case manufacturer
+        when "Philips Medical Systems"
+          result = pms_get_all_measurements(dcm)
+        when "GE Medical Systems"
+          result = gms_get_all_measurements(dcm[CS])
+        when "GE Healthcare"
+          result = gh_get_all_measurements(dcm[CS])
+        else
+          status = { error: "1", message: "#{manufacturer} is not supported yet." }
+        end
+
+        result = standardize_result(result)
+      end
+
+      if result.nil?
+        status = {
+          error: 1,
+          message: "No fetchable data in this SR."
+        }
       end
     end
 
     content_type :json
-    { status: status, result: standardize_result(result) }.to_json
+    { status: status, result: result }.to_json
   end
+
+
 end
