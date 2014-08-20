9dac13328ef78db23829da22c0b9f64bb8c49061
I-Ta Tsai@MBP <ittsai@gmail.com>
2014-08-20 22:13:59 +0800

    crop image manually. it improves the ocr performance very much.

--- app/ocr-seg.rb
+++ app/ocr-seg.rb
@@ -1,4 +1,5 @@
 require 'rtesseract'
+require 'RMagick'
 
 class RTesseract
   # Class to read an image from specified areas
@@ -32,27 +33,39 @@ class RTesseract
 end
 
 def ocr_seg(path)
-  # very inefficient
-  mix_block = RTesseract::MyMixed.new(path) do |image|
-    image.labeled_area(470, 870, 68, 41, "RtABI")
-    image.labeled_area(730, 870, 68, 41, "LtABI")
+  # crop image to small
+  img = Magick::Image.read(path).first
+  areas = [
+    { x: 470, y: 870, w: 68, h: 41, l: "RtABI"},
+    { x: 730, y: 870, w: 68, h: 41, l: "LtABI"},
 
-    image.labeled_area(690, 1068, 57, 25, "RtBrachial")
-    image.labeled_area(755, 1068, 57, 25, "LtBrachial")
+    { x: 690, y: 1068, w: 57, h: 25, l: "RtBrachial" },
+    { x: 755, y: 1068, w: 57, h: 25, l: "LtBrachial" },
 
-    image.labeled_area(690, 1128, 57, 25, "RtUpperThigh")
-    image.labeled_area(755, 1128, 57, 25, "LtUpperThigh")
+    { x: 690, y: 1128, w: 57, h: 25, l: "RtUpperThigh" },
+    { x: 755, y: 1128, w: 57, h: 25, l: "LtUpperThigh" },
 
-    image.labeled_area(690, 1187, 57, 25, "RtLowerThigh")
-    image.labeled_area(755, 1187, 57, 25, "LtLowerThigh")
+    { x: 690, y: 1187, w: 57, h: 25, l: "RtLowerThigh" },
+    { x: 755, y: 1187, w: 57, h: 25, l: "LtLowerThigh" },
 
-    image.labeled_area(690, 1245, 57, 25, "RtCalf")
-    image.labeled_area(755, 1245, 57, 25, "LtCalf")
+    { x: 690, y: 1245, w: 57, h: 25, l: "RtCalf" },
+    { x: 755, y: 1245, w: 57, h: 25, l: "LtCalf" },
 
-    image.labeled_area(690, 1305, 57, 25, "RtAnkle")
-    image.labeled_area(755, 1305, 57, 25, "LtAnkle")
+    { x: 690, y: 1305, w: 57, h: 25, l: "RtAnkle" },
+    { x: 755, y: 1305, w: 57, h: 25, l: "LtAnkle" }
+  ]
+
+  ocr_result = {}
+
+  areas.each do |area|
+    f = Tempfile.new([area[:l], ".tif"])
+    img.crop(area[:x], area[:y], area[:w], area[:h]).write(f.path)
+    txt_f = Tempfile.new(area[:l])
+    `tesseract "#{f.path}" "#{txt_f.path}" 2>/dev/null`
+    ocr_result[area[:l]] = File.read(txt_f.path + ".txt").to_s
+    f.unlink
+    txt_f.unlink
   end
-  ocr_result = mix_block.to_s_with_label
 
   ocr_result.each do |k, v|
     ocr_result[k] = (v.to_i > 2) ? v.to_i.to_s : ("%.2f" % v.to_f) if v.length > 0
