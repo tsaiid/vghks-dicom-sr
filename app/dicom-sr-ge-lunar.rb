c0c7468573c89f88427e79812ccac1f3a834fcb2
I-Ta Tsai@MBPR <ittsai@gmail.com>
2015-01-07 12:38:07 +0800

    add a method to get mp age by ocr.

--- app/dicom-sr-ge-lunar.rb
+++ app/dicom-sr-ge-lunar.rb
@@ -46,3 +46,41 @@ def gelunar_all_measurements(items)
 
   results.empty? ? nil : results
 end
+
+def gelunar_get_mp_age(dcm)
+  unless dcm.nil?
+    status, dcms = get_dcm_by_acc_no(dcm[AN].value, "OT", 1)
+    if dcms.length > 0
+      fg = Tempfile.new('gdcm')
+      fd = Tempfile.new('dcm')
+      dcms.first.write(fd.path)
+      `gdcmconv -w #{fd.path} #{fg.path}`
+      fd.unlink
+
+      img = Magick::Image.read(fg.path).first
+      fg.unlink
+
+      # crop
+      fc = Tempfile.new(['ocr_bd_crop', '.tif'])
+      img.crop(476, 203, 152, 16).scale(2).write(fc.path)
+    #p fc.path
+    # ocr
+      txt_f = Tempfile.new('ocr_bd_txt')
+      `tesseract "#{fc.path}" "#{txt_f.path}" 2>/dev/null`
+    #p txt_f.path
+      ocr_result = File.read(txt_f.path + ".txt").to_s
+    # manipulate text
+      mp_age = nil
+      ocr_result.match(/Menopause Age: ([\w\d]+)/) do |m|
+        mp_age = m[1]
+        # correct "so" to 50
+        mp_age = "50" if mp_age == "so"
+      end
+
+      txt_f.unlink
+      fc.unlink
+    end
+  end
+
+  mp_age.empty? ? nil : mp_age
+end
